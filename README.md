993433791bd7371e2041c26843b40d80224c934e
Mpo Mpo <86723923+XFRANZZ@users.noreply.github.com>
2021-07-01 11:01:22 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+## KK & KTP Generator
+# Command Install For Termux / Linux (pc)
+```
+$ pkg install php
+$ pkg install git
+$ git clone https://github.com/XFRANZZ/kk-ktp
+$ cd kkktp
+$ ls
+$ php kkktp.php
+```
+
+# Thanks To
+PEMALANG-CYBER-TEAM || IndoXploit
---
+++ var.php
@@ -0,0 +1,34 @@
+<?php
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function kkktp_banner(){
+  echo "\e[91;1m
+       __   __         __   __
+        / /__/ /__      / /__/ /_____
+       / //_/ //_/_____/ //_/ __/ __ \
+      / ,< / ,< /_____/ ,< / /_/ /_/ /
+     /_/|_/_/|_|     /_/|_|\__/ .___/
+                             /_/
+                                V 2.1
+  \n";
+}
+?>
